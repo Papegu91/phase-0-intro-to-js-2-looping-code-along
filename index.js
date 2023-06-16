1cbb5aeefd1c32d98de07b06e82a41d46289023a
peter gudoi <petergudoi@gmail.com>
2023-06-16 20:16:29 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(name, event){
+    let messageArr = [];
+    for(let i=0; i<name.length; i++){
+        let message=`Thank you, ${name[i]}, for the wonderful ${event} gift!`
+        messageArr.push(message);
+    }
+    return messageArr;
+}
+
+function countDown(i){
+    while(i>=0){
+        console.log(i);
+        i--;
+    }
+}
+console.log(countDown(10));
+
